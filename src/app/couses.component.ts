d560522a5eede0303b53f5800859fe09e0a37615
Nuang suri <suriya.v@abctech.co.th>
2017-09-25 17:34:41 +0700

    commit after reset

--- src/app/couses.component.ts
+++ src/app/couses.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-      selector: 'course',
-      template: '<h2>Courses</h2>'
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
-
-export class CoursesComponent {
-
+export class AppComponent {
+ title = 'app';
 }
